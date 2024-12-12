8977e30677c878cddfc0a43d95980d607989bc60
Shandea <86807815+Shandea@users.noreply.github.com>
2024-12-12 12:15:16 -0800

    updated blurb

--- src/Views/Components/Blurb.jsx
+++ src/Views/Components/Blurb.jsx
@@ -1,13 +1,8 @@
 const Blurb = ({ text, textColor }) => {
   return (
-    <div className="w-1/2">
-      <p className="font-serif w-1/2 p-5 text-center">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero tenetur
-        numquam accusantium beatae similique, totam, maiores quaerat natus quos
-        modi laborum ad pariatur sapiente aliquid praesentium sint fugiat quia
-        voluptates
-      </p>
-    </div>
+    <>
+      <p className={`p-5 p text-center text-${textColor}`}>{text}</p>
+    </>
   );
 };
 
