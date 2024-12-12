8adf635b4178d55a2c5550f62c3998ff652b2c60
megger24 <meggerhoran@gmail.com>
2024-12-12 10:39:18 -0700

    Button

--- src/Views/Components/Button.jsx
+++ src/Views/Components/Button.jsx
@@ -1,31 +1,14 @@
-import React from 'react'
-const Button = ({
-    text,
-    onClick,
-    backgroundColor = "#d63d52",
-    color = '#fceced',
-    style = {},
-    disabled = false,
-  }) => {
-    
-    const baseStyles = {
-      backgroundColor,
-      color,
-      fontSize: '16px',
-      fontWeight: '500',
-      padding: '10px 20px',
-      border: 'none',
-      borderRadius: '8px',
-      cursor: disabled ? 'not-allowed' : 'pointer',
-      opacity: disabled ? 0.6 : 1,
-      ...style, 
-    };
-  
-    return (
-      <button style={baseStyles} onClick={onClick} disabled={disabled}>
-        {text}
-      </button>
-    );
-  };
-  
+import React from "react";
+
+const Button = ({ text, onClick, backgroundColor, textColor }) => {
+  return (
+    <button
+      className={`bg-${backgroundColor} text-${textColor} font-medium py-2 px-4 rounded-lg `}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
+};
+
 export default Button;
