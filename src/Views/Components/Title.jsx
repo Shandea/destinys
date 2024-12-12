97871d79d0b93e6083e3cb7ff4f64cd9f7149d58
Shandea <86807815+Shandea@users.noreply.github.com>
2024-12-12 10:26:39 -0800

    formatted and added h1 class to title component

--- src/Views/Components/Title.jsx
+++ src/Views/Components/Title.jsx
@@ -1,11 +1,11 @@
-const Title = ({text})=>{
-    return(
-        <div className="flex justify-center">
-        
-        <h1 className="bg-secondaryRed border-y-4 border-accentAqua p-3 text-white text-center w-1/2">{text}</h1>
-        </div>
-    );
-}
+const Title = ({ text }) => {
+  return (
+    <div className="flex justify-center">
+      <h1 className="h1 bg-secondaryRed border-y-4 border-accentAqua p-3 text-white text-center w-full">
+        {text}
+      </h1>
+    </div>
+  );
+};
 
-
-export default Title
+export default Title;
