c323b6319118614d693315d9917053df95c10055
ShealiSunshine <mandiksheali@gmail.com>
2024-12-12 12:55:37 -0700

    Blurb.jsx

---
+++ src/Views/Components/Blurb.jsx
@@ -0,0 +1,14 @@
+const Blurb = ({ text, textColor }) => {
+  return (
+    <div className="w-1/2">
+      <p className="font-serif w-1/2 p-5 text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero tenetur
+        numquam accusantium beatae similique, totam, maiores quaerat natus quos
+        modi laborum ad pariatur sapiente aliquid praesentium sint fugiat quia
+        voluptates
+      </p>
+    </div>
+  );
+};
+
+export default Blurb;
