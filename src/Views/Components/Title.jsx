e5e9bd747a83c9cc7cb96e87d96704104484d925
ShealiSunshine <mandiksheali@gmail.com>
2024-12-12 10:34:15 -0700

    Title.jsx

---
+++ src/Views/Components/Title.jsx
@@ -0,0 +1,11 @@
+const Title = ({text})=>{
+    return(
+        <div className="flex justify-center">
+        
+        <h1 className="bg-secondaryRed border-y-4 border-accentAqua p-3 text-white text-center w-1/2">{text}</h1>
+        </div>
+    );
+}
+
+
+export default Title
