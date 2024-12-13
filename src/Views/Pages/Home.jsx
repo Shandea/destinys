d59191de03ad9ce4d1c7a222f95b4f068ae9a8d7
Shandea <86807815+Shandea@users.noreply.github.com>
2024-12-13 12:18:27 -0800

    bug updates

--- src/Views/Pages/Home.jsx
+++ src/Views/Pages/Home.jsx
@@ -1,12 +1,7 @@
-import Button from "../Components/Button";
-import Blurb from "../Components/Blurb";
-
 const Home = () => {
   return (
     <>
       <h1>Home</h1>
-      <Blurb text="Welcome to our site!" textColor="text-primaryBlue" />
-      <Button text="Click Me" backgroundColor="bg-secondaryRed" textColor="text-primaryBlue" />
     </>
   );
 };
