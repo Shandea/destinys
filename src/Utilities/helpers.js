4b3c83c23115364f678f03016fa7e0fcc3919249
megger24 <meggerhoran@gmail.com>
2024-12-16 09:30:33 -0700

    Cake

--- src/Utilities/helpers.js
+++ src/Utilities/helpers.js
@@ -2,6 +2,13 @@ import cakeBlue from "../Assets/Images/CakeBlue.png";
 import cookieChristmas from "../Assets/Images/CookieChristmas.png";
 import cookieBrownie from "../Assets/Images/CookieBrownies.png";
 import cakeWedding from "../Assets/Images/CakeWedding.png";
+import cakeChocolate from "../Assets/Images/CakeChocolate.png";
+import cakeDonut from "../Assets/Images/CakeDonut.png";
+import cakeFruit from "../Assets/Images/CakeFruit.png";
+import cakeHeart from "../Assets/Images/CakeHeart.png"
+import CheeseCake from "../Assets/Images/CheeseCake.png";
+import CakeBirthday from "../Assets/Images/CakeBirthday.png"
+
 
 const data = {
   flipCardData: [
@@ -32,6 +39,48 @@ const data = {
       src: cookieBrownie,
       alt: "A cookie brownie with topping",
       id: 4,
+    },
+    {
+      title: "Celebrations",
+      text: "From birthdays to anniversaries, we create sweet moments for your most cherished celebrations.",
+      src: CakeBirthday,
+      alt: "A fun birthday cake that has colorful frosting ",
+      id: 5,
+    },
+    {
+      title: "Celebrations",
+      text: "From birthdays to anniversaries, we create sweet moments for your most cherished celebrations.",
+      src: CheeseCake,
+      alt: "A delicious gourmet cheese cake ",
+      id: 6,
+    },
+    {
+      title: "Celebrations",
+      text: "From birthdays to anniversaries, we create sweet moments for your most cherished celebrations.",
+      src: cakeHeart,
+      alt: "A delicious gourmet red velvet cake ",
+      id: 7,
+    },
+    {
+      title: "Celebrations",
+      text: "From birthdays to anniversaries, we create sweet moments for your most cherished celebrations.",
+      src: cakeDonut,
+      alt: "A delicious gourmet white cake with donuts cascading down the cake ",
+      id: 8,
+    },
+    {
+      title: "Celebrations",
+      text: "From birthdays to anniversaries, we create sweet moments for your most cherished celebrations.",
+      src: cakeChocolate,
+      alt: "A delicious gourmetdark chocolate cake for any chocolate lovers ",
+      id: 9,
+    },
+    {
+      title: "Celebrations",
+      text: "From birthdays to anniversaries, we create sweet moments for your most cherished celebrations.",
+      src: cakeFruit,
+      alt: "A delicious gourmetdark fruit cake with assortments of fresg fruit ",
+      id: 10,
     },
   ],
   servicesBlurb: "At Destiny's Bakery, we offer a delightful selection of everyday treats, including cakes, doughnuts, cookies, kolache's, and freshly brewed coffee, made fresh daily. Beyond our daily offerings, we specialize in event catering and custom orders for holidays, birthdays, anniversaries, and graduations. We're also proud to support our community by hosting and providing goods for fundraising events. Whether it's a quick treat, a special occasion, or a community gathering, Destiny's Bakery is here to make every moment sweeter!"
--- src/Views/Pages/Cake.jsx
+++ src/Views/Pages/Cake.jsx
@@ -1,8 +1,36 @@
+import React from "react";
+import Button from "../Components/Button.jsx";
+import Header from "../Components/Header.jsx";
+import Blurb from "../Components/Blurb.jsx";
+import Image from "../Components/Image.jsx";
+import data from '../../Utilities/helpers.js'; 
+
+
 const Cake = () => {
+  const desiredOrder = [3, 1, 5, 7, 6,8, 9,10 ];
+  const imagesToDisplay = desiredOrder
+  .map((id) => data.flipCardData.find((item) => item.id === id)) 
+  .filter((image) => image); 
   return (
     <>
-      <h1>Cake</h1>
+      <Header />
+      <Blurb 
+        text="From elegant wedding cakes to fun birthday designs, our cakes are made to celebrate life's sweetest moments. Each one is handcrafted with love and customized to your vision, ensuring your special day is as unique as you are"
+      />
+      <Button className="color-#d63d52" />
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-6 mt-8">
+        {imagesToDisplay.map((image) => (
+          <div key={image.id} className="image-item">
+            <Image
+              src={image.src}
+              alt={image.alt}
+              className="border-accentAqua border-4 rounded-3xl h-full w-full object-contain"
+            />
+          </div>
+        ))}
+      </div>
     </>
   );
 };
+
 export default Cake;
