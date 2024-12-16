8861be2455c084e7c840dc34d474045938298c7c
ObserveEveryMove <kellerpatrick01@gmail.com>
2024-12-16 09:11:03 -0700

    completed per requirements outlined in ticket for home page

--- src/Views/Pages/Home.jsx
+++ src/Views/Pages/Home.jsx
@@ -1,7 +1,23 @@
+import Title from "../Components/Title";
+import Blurb from "../Components/Blurb";
+import Image from "../Components/Image";
+import cookies from "../../Assets/Images/CookieWhiteChocolateSideWays.png";
+import cupCakePink from "../../Assets/Images/CupcakePink.png";
+const homeBlurbText =
+  "At Destiny's Bakery, our mission is to spread joy and create moments of happiness through our love for baking. We are committed to making every treat a delightful experience while giving back to the community by donating to our neighborhood and helping raise funds for those in need.";
 const Home = () => {
   return (
     <>
-      <h1>Home</h1>
+      <img
+        src={cookies}
+        alt="white choclate strawberry cookies"
+        className="h-full w-full object-contain"
+      />
+      <Title text="Our Mission" />
+      <div className="my-4 flex flex-col md:flex-row content-center items-center p-5 border-accentAqua border-y-2">
+        <Blurb text={homeBlurbText} />
+        <Image src={cupCakePink} alt="pink and brown cupcake" />
+      </div>
     </>
   );
 };
