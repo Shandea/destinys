d1311a51d80faed6272f541f28b1412a131d86a2
megger24 <meggerhoran@gmail.com>
2024-12-17 13:35:36 -0700

    Footer

--- src/Views/Pages/Layout.jsx
+++ src/Views/Pages/Layout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router";
 import Navbar from "../Components/NavBar";
+import Footer from "../Components/Footer"
 
 const Layout = () => {
   return (
     <div div className="w-full">
       <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 };
